85b230ac17a437630f71083e246688c4eefef37c
David Enyeart <enyeart@davids-mbp.raleigh.ibm.com>
2023-12-05 13:36:19 -0500

    FVT debug
    Signed-off-by: David Enyeart <enyeart@davids-mbp.raleigh.ibm.com>

--- docker-env.mk
+++ docker-env.mk
@@ -21,7 +21,7 @@ ifneq ($(NO_PROXY),)
 DOCKER_BUILD_FLAGS+=--build-arg NO_PROXY=$(NO_PROXY)
 endif
 
-DBUILD = docker build $(DOCKER_BUILD_FLAGS)
+DBUILD = docker build --no-cache --progress=plain $(DOCKER_BUILD_FLAGS)
 
 DOCKER_NS ?= hyperledger
 DOCKER_TAG=$(ARCH)-$(PROJECT_VERSION)
